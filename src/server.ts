21beb4bfe3427bba66682f696c01209cabfda357
AndreLucasTrevizan <trevizan.al@gmail.com>
2023-03-02 22:42:12 -0300

    Remove Morgan

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,12 @@
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
-import morgan from 'morgan';
 import router from './router';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(morgan('dev'));
 app.use(router);
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof Error) return res.status(400).json({ error: err.message });
